f4b743cabf653f78f9b48348a72876dcf10b21a1
antony <antony@gronk.tewa.local>
2016-12-24 18:26:57 +0000

    Initial commit

---
+++ src/com/strom/Item.java
@@ -0,0 +1,8 @@
+package com.strom;
+
+/**
+ * Created by antony on 24/12/16.
+ * base class for all types of items
+ */
+public class Item {
+}
---
+++ src/com/strom/Main.java
@@ -0,0 +1,8 @@
+package com.strom;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
