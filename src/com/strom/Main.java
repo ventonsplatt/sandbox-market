934eb7f90485979973c44aaf536ab76307a789d4
antony <antonygooch2@gmail.com>
2016-12-31 10:24:56 +0000

    move item added

--- src/com/strom/Main.java
+++ src/com/strom/Main.java
@@ -6,12 +6,21 @@ public class Main {
     Warehouse warehouse1 = new Warehouse();
 
     Item item1 = new Item("A Cheese sandwich");
-    Item item2 = new Item("A ham sandwich");
-    Item item3 = new Item("An egg sandwich");
-    Item item4 = new Item("A hummus sandwich");
+    Item item2 = new Item("A Cheese sandwich");
+    Item item3 = new Item("A Cheese sandwich");
+    Item item4 = new Item("A Cheese sandwich");
     Item item5 = new Item("A Cheese sandwich");
     Item item6 = new Item("A Cheese sandwich");
-    Item item7 = new Item("A cheese and tomato sandwich");
+    Item item7 = new Item("A Cheese sandwich");
+    Item item8 = new Item("A Cheese sandwich");
+    Item item9 = new Item("A Cheese sandwich");
+    Item item10 = new Item("A Cheese sandwich");
+    Item item11 = new Item("A Cheese sandwich");
+
+     //   "A hummus sandwich"
+        //  "An egg sandwich"
+        //"A ham sandwich"
+        //"A cheese and tomato sandwich"
 
     item1.setItemPrice(23.5d);
     warehouse1.addWarehouseItem(item1);
@@ -21,17 +30,24 @@ public class Main {
     warehouse1.addWarehouseItem(item5);
     warehouse1.addWarehouseItem(item6);
     warehouse1.addWarehouseItem(item7);
+    warehouse1.addWarehouseItem(item8);
+    warehouse1.addWarehouseItem(item6);
+    warehouse1.addWarehouseItem(item7);
+    warehouse1.addWarehouseItem(item8);
+    warehouse1.addWarehouseItem(item9);
+    warehouse1.addWarehouseItem(item10);
+    warehouse1.addWarehouseItem(item11);
 
 //        System.out.println(item1.itemName);
 
     System.out.println(warehouse1.getWarehouseItemCount());
 
-        for(int i=0; i < warehouse1.getWarehouseItemCount(); i++) {
-            System.out.println("i:" + i + " contains " + warehouse1.getWarehouseItem(i).getItemName());
-        }
+        //for(int i=0; i < warehouse1.getWarehouseItemCount(); i++) {
+        //    System.out.println("i:" + i + " contains " + warehouse1.getWarehouseItem(i).getItemName());
+        //}
 
         // remove an element
-        warehouse1.removeWarehouseItem(4);
+        //warehouse1.removeWarehouseItem(4);
 
         for(int i=0; i < warehouse1.getWarehouseItemCount(); i++) {
             System.out.println("i:" + i);
@@ -41,6 +57,19 @@ public class Main {
 
         // find a cheese sandwich
         System.out.println("looking for cheese:" + warehouse1.getStockCount("A Cheese sandwich"));
+
+        // get the key values for the search item
+        System.out.println("item indexes");
+        //System.out.println(warehouse1.getItemIndex("A Cheese sandwich", 1));
+        //System.out.println(warehouse1.getItemIndex("A Cheese sandwich", 2));
+        System.out.println(warehouse1.getItemIndex("A Cheese sandwich", 3));
+        //System.out.println(warehouse1.getItemIndex("A Cheese sandwich", 4));
+        //System.out.println(warehouse1.getItemIndex("A Cheese sandwich", 5));
+        System.out.println("end of item indexes");
+
+        // moving an item
+        warehouse1.moveWarehouseItem(warehouse1.getItemIndex("A Cheese sandwich", 3), "a house");
+
 
     }
 
--- src/com/strom/Warehouse.java
+++ src/com/strom/Warehouse.java
@@ -1,4 +1,5 @@
 package com.strom;
+import javax.swing.*;
 import java.util.*;
 /**
  * Created by antony on 26/12/16.
@@ -31,6 +32,19 @@ public class Warehouse {
         // returns the item location name
         return warehouseLocations.get(itemIndex);
     }
+    public int getWarehouseLocationIndex(String warehouseLocation){
+        // returns the index based on inputted warehouse location
+        for(int i =0; i < 500; i++) {
+            if (getWarehouseLocation(i) == warehouseLocation) {
+                // matching location found
+                return i;
+            } else {
+                // no matching location
+                return -1;
+            }
+        }
+    return 0;
+    }
 
     public void addWarehouseItem(Item item){
         warehouseItems.add(item);
@@ -41,7 +55,47 @@ public class Warehouse {
         warehouseItems.set(itemIndex, myEmptyItem); //put an empty thing in it
     }
 
-    public int getStockCount(String itemName) {
+  public void moveWarehouseItem(int itemIndex, String newLocation) {
+      // check that: a: searched item exists
+      //             b: stocklocation is available
+
+      //if((warehouseItems.get(itemIndex).getItemName() == itemName) && )
+      // Item myEmptyItem = new Item("");
+      System.out.println("moving item from slot:" + itemIndex);
+
+  }
+
+
+
+        //warehouseItems.set(itemIndex, myEmptyItem); //put an empty thing in it
+    //}
+
+    public int getItemIndex(String itemName, int targetItemInstance) {
+        // this helper function simply returns the nth itemIndex for the search time
+        int itemInstance = 0;
+        int itemIndex = 0;
+        System.out.println("Looking for " + targetItemInstance + "th item");
+
+        for (int i = 0; i <= warehouseItems.size(); i++) {
+            // check if name matches and  increment itemInstance count
+            if (warehouseItems.get(i).getItemName() == itemName) {
+                itemInstance += 1;
+                itemIndex = i;
+               // System.out.println("Matched itemName...Current value of itemInstance:" + itemInstance);
+               // System.out.println("Not matched...Current value of itemIndex:" + itemIndex);
+
+                if(itemInstance == targetItemInstance){
+                    return itemIndex;
+                }
+            } else {
+                System.out.println("") ;
+                //System.out.println("Not matched...Current value of i:" + i);
+            }
+        }
+    return 0;
+    }
+
+        public int getStockCount(String itemName) {
         // iterates through the items held in the warehouse and returns a count of the objects matching by name
         int itemCount = 0;
         for (int i = 0; i < warehouseItems.size(); i++) {
@@ -51,6 +105,10 @@ public class Warehouse {
             }
         }
         return itemCount;
+    }
+
+    private void handleNoFreeSlot(){
+        System.out.println("ERROR: Cannot move item - move cancelled");
     }
 
 }
